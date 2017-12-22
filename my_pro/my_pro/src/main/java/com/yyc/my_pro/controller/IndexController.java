e0c56cd86dc0b570cb45ca9edf8e48b238490c7d
Givemeablock <yangyuanchangdali@163.com>
2017-12-22 14:27:56 +0800

    freemaker的list循环语法

--- my_pro/my_pro/src/main/java/com/yyc/my_pro/controller/IndexController.java
+++ my_pro/my_pro/src/main/java/com/yyc/my_pro/controller/IndexController.java
@@ -1,10 +1,14 @@
 package com.yyc.my_pro.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
 
 //import java.lang.reflect.Method;
 
@@ -30,7 +34,11 @@ public class IndexController {
     }
 
     @RequestMapping("/ftl")
-    public String news() {
+    //此处的ftl文件后缀必须为ftl,不能是html
+    public String news(Model model) {
+        model.addAttribute("value1", "i am value1");
+        List<String> colors = Arrays.asList(new String[]{"RED", "BLUE", "YELLOW"});
+        model.addAttribute("colors", colors);
         return "news";
     }
 }
--- my_pro/my_pro/src/main/resources/templates/news.ftl
+++ my_pro/my_pro/src/main/resources/templates/news.ftl
@@ -6,5 +6,12 @@
 </head>
 <body>
     <h1>This is a free-maker based page</h1>
+    <div>i am value1: ${value1}</div>
+    <ul>
+      <#list colors as color>
+          <li>${color}</li>
+      </#list>
+    </ul>
+
 </body>
 </html>
